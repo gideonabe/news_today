00868df730a671e5b3ad46b0beea429e2809b024
Gideon Abe <gideonabe2020@gmail.com>
2025-11-12 16:12:44 +0100

    First commit - Navbar component implemented

---
+++ components/Navbar.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Bell, CalendarRange, Moon, Search } from "lucide-react";
+
+const Navbar = () => {
+  return (
+    <nav className='pb-3 mt-3 border-b border-gray-400'>
+      <div className='flex justify-between max-w-[95%] mx-auto'>
+        <div className='flex gap-18'>
+          <div className='flex items-center gap-0.1'>
+            <CalendarRange color='blue'/>
+            <h1 className='font-semibold text-2xl'>NewsToday</h1>
+          </div>
+
+          <ul className='flex items-center gap-6 font-medium'>
+            <li>Top Stories</li>
+            <li>World</li>
+            <li>Politics</li>
+            <li>Business</li>
+            <li>Tech</li>
+            <li>Culture</li>
+          </ul>
+        </div>
+
+        <div className='flex items-center gap-4'>
+          <div className='relative'>
+            <Search className='absolute left-3 top-1/2 -translate-y-1/2 text-gray-600'/>
+            <input type="text" className='bg-gray-200 py-2 pl-10 rounded-full focus:outline-none'/>
+          </div>
+          <Moon />
+          <Bell />
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
